9b93c892ad71b84f68799ec25fe25b7aa00b2f26
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-01-11 15:42:47 -0600

    Learn forEach loop for arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,6 +71,82 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+/* SLICE */
+console.log(arr.slice(1, 3));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -2));
+// You can use slice to create a shallow copy of an array -> Useful for chaining methods
+console.log(arr.slice()); // empty parameter
+// Same as the spread operator:
+console.log([...arr]);
+
+/* SPLICE */
+// Unlike slice, the original array gets mutated.
+// We "remove" the section of the array.
+console.log(arr.splice(2)); // Extracts ['c', 'd', 'e']
+console.log(arr); // Remains ['a', 'b']
+
+// Second parameter is the "deleteCount"
+arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.splice(1, 2)); // Start at position 1 and delete 2 items
+console.log(arr);
+
+/* REVERSE */
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2);
+console.log(arr2.reverse());
+console.log(arr2);
+
+/* CONCAT */
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+/* JOIN */
+const joinedLetters = letters.join(', ');
+console.log(joinedLetters);
+
+/* ES2022 the new "at(index)" method */
+const arr3 = [23, 11, 64];
+console.log(arr3[0]);
+console.log(arr3.at(0));
+
+/* Traditional way of getting the last element in the array */
+console.log(arr3[arr3.length - 1]); // arr3[-1] doesn't work
+console.log(arr3.slice(-1)[0]);
+
+/* Modern way using at(index) */
+console.log(arr3.at(-1)); // Yep this is very useful, and more intuitive!
+
+/* Looping over Arrays: forEach */
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/* for of version */
+console.log('-----FOR OF-----');
+for (const [index, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+/* Using forEach (forEach is a higher order function that takes a callback function as parameter)*/
+console.log('-----FOR EACH-----');
+movements.forEach(function (movement, index, order) {
+  // Parameters for callback function:
+  // 1: item, 2: index, 3: array
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
+// You can't use break/continue in forEach
+
+/* forEach with Maps and Sets */
