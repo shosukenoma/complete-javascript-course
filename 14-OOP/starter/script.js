1c436c21262fbb99c92ef3908243bb820d3a3571
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-01-13 09:58:06 -0600

    Complete Coding Challenge #2

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -238,3 +238,48 @@ const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1987);
 console.log(sarah);
 console.log(sarah.__proto__);
+
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+*/
+
+class ClassCar {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`Accelerated. You are now going at ${this.speed} km/h.`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`Braked. You are now going at ${this.speed} km/h.`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new ClassCar('Ford', 120);
+console.log(ford.speedUS);
+
+ford.accelerate();
+ford.brake();
+
+ford.speedUS = 30;
+console.log(ford.speedUS);
