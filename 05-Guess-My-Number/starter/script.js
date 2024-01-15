b92080803bd58726af06bfeaf7e3abbb97ec5890
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-01-15 14:27:41 -0600

    Implement Game Logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,44 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 98;
+// document.querySelector('.score').textContent = 12;
+
+// document.querySelector('.guess').value = 23;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number Entered!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lose!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lose!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
