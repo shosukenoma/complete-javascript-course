b568841c13e87d58b2ed7720129e0d5fe36699c1
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-01-15 14:37:30 -0600

    Learn about manipulating CSS styles

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -21,23 +21,25 @@ document.querySelector('.check').addEventListener('click', () => {
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number Entered!';
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct!';
+    document.querySelector('.message').textContent = '🎉 Correct!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too High!';
+      document.querySelector('.message').textContent = '📈 Too High!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You Lose!';
+      document.querySelector('.message').textContent = '💣 You Lose!';
       document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too Low!';
+      document.querySelector('.message').textContent = '📉 Too Low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You Lose!';
+      document.querySelector('.message').textContent = '💣 You Lose!';
       document.querySelector('.score').textContent = 0;
     }
   }
