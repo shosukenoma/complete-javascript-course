594930a7cb052930c310e025a302e5d9180e4ba4
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-01-16 17:40:43 -0600

    Complete Coding Challenge #3

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -344,6 +344,8 @@ const calcAverageHumanAge = function (dogAges) {
 };
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 
+////////////////////////////////////////////
+
 const totalDepositsUSD = movements
   .filter(mov => mov > 0)
   .map(mov => mov * eurToUsd)
@@ -364,3 +366,24 @@ console.log(totalDepositsUSD);
 //   .reduce((acc, mov) => acc + mov, 0);
 
 // console.log(totalDepositsUSD);
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const chainCalcAverageHumanAge = function (dogAges) {
+  const average = dogAges
+    .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  console.log(average);
+};
+chainCalcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
