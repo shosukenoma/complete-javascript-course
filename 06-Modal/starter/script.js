4af0c97c852102f053c44da27dafad8556b3d5d3
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-01-15 20:59:30 -0600

    Complete modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+const showModal = function () {
+  console.log('Button clicked!');
+  // Manually remove class attributes.
+  // !! NO DOT in the beginning !!
+  overlay.classList.remove('hidden');
+  modal.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', showModal);
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// Esc Key Press Event
+document.addEventListener('keydown', function (e) {
+  // console.log(e);
+  if (!modal.classList.contains('hidden') && e.key === 'Escape') {
+    closeModal();
+    console.log('The esc key was pressed.');
+  }
+});
