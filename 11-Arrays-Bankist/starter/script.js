e965fc254e8b3ac84b7e2388a50e27791be1e038
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-01-17 13:34:09 -0600

    Implement transfer functionality

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -80,7 +80,7 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-displayMovements(account1.movements);
+// displayMovements(account1.movements);
 
 // MAP method
 const createUsernames = function (accs) {
@@ -98,31 +98,90 @@ createUsernames(accounts);
 console.log(accounts);
 
 // REDUCE method
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, cur) => acc + cur, 0);
-  labelBalance.textContent = `${balance} EUR`;
-};
-calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const ins = movements
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, cur) => acc + cur, 0);
+  labelBalance.textContent = `${acc.balance} €`;
+};
+// calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (acc) {
+  const ins = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${ins}€`;
 
-  const outs = movements
+  const outs = acc.movements
     .filter(mov => mov < 0)
-    .reduce((arr, mov) => arr + mov, 0);
+    .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(outs)}€`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(mov => mov * 0.012)
+    .map(mov => mov * (acc.interestRate / 100))
     .filter(mov => mov >= 1)
-    .reduce((arr, mov) => arr + mov, 0);
+    .reduce((acc, mov) => acc + mov, 0);
   labelSumInterest.textContent = `${interest}€`;
 };
-calcDisplaySummary(account1.movements);
+// calcDisplaySummary(account1.movements);
+
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+  // Display balance
+  calcDisplayBalance(acc);
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting / Prevent refresh
+  e.preventDefault();
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  // Optional chaining, in case currentAccount = undefined.
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and Welcome Message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputTransferAmount.value);
+
+  const recipient = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+
+  if (
+    amount > 0 &&
+    currentAccount.balance >= amount &&
+    recipient?.username &&
+    recipient?.username !== currentAccount.username
+  ) {
+    console.log('Transfer Valid!');
+    currentAccount.movements.push(-Number(inputTransferAmount.value));
+    recipient.movements.push(amount);
+    updateUI(currentAccount);
+  }
+
+  inputTransferAmount.value = inputTransferTo.value = '';
+});
 
 // Lecture Notes
 
@@ -393,5 +452,5 @@ console.log(movements);
 console.log(firstWithdrawal);
 
 console.log(accounts);
-const account = accounts.find(acc => (acc.owner = 'Jessica Davis'));
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 console.log(account);
