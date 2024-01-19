7aa9f4e50af3bd5237a0414aa45f3b7296803603
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-01-18 22:35:03 -0600

    Implement bankist sorting feature

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,11 +61,14 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   // Remove HTML content
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  // Create a shallow copy
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal ';
     const html = `
       <div class="movements__row">
@@ -217,6 +220,13 @@ btnClose.addEventListener('click', function (e) {
 
     inputCloseUsername.value = inputClosePin.value = '';
   }
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  sorted = !sorted;
+  e.preventDefault();
+  displayMovements(currentAccount.movements, sorted);
 });
 
 // Lecture Notes
