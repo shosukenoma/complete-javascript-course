eb97da44834702bdfbdd18b96c06ce643ea4ee37
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-01-21 12:27:36 -0600

    Learn about selecting, creating and deleting elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -31,3 +31,52 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
+
+// Selects the whole HTML document (<head><body>etc.)
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+
+// This gets all elements with a certain <tagName>
+// This method returns an HTLMCollection
+// - A "Live" Collection that gets updated automatically by insert/delete of tags
+// - NodeList doesn't get updated so keeps the original state.
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and Inserting Elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message); // Inserts as a child of the header element
+header.append(message); // Inserts after the header element
+// These can only be implemented one at a time.
+
+// In order to use one element in two places, we use
+// message.cloneNode(true) //true -> All child elements will be copied as well.
+
+// header.before(message) // Insert as the sibling of header
+// header.after(message)
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
