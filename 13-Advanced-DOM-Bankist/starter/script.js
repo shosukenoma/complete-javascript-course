86a674fbef04373d739914b40b73c44777d77435
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-01-23 16:30:07 -0600

    Learn about the "scroll" event

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -138,6 +138,20 @@ const handleHover = function (e, opacity) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// Sticky navigation
+
+// Using the 'scroll' event is very bad for performance
+// as it fires up the event constantly
+const initialCoords = section1.getBoundingClientRect();
+window.addEventListener('scroll', function () {
+  console.log(initialCoords);
+  if (window.scrollY > initialCoords.top) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
+
 ////////////////////////////////////
 ////////////////////////////////////
 ////////////////////////////////////
